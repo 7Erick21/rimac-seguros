e7ede2ec985d1ca85a33236c35588b247cc3f827
pinedacuevaerick910 <pinedacuevaerick910@gmail.com>
2023-12-18 12:50:58 -0500

    Meta OG SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Helmet, HelmetData } from 'react-helmet-async'
+import { Helmet, HelmetData, HelmetProvider } from 'react-helmet-async'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { RouterProvider } from 'react-router-dom'
 
@@ -10,37 +10,39 @@ const helmetData = new HelmetData({})
 
 const App = () => {
     return (
-        <QueryClientProvider client={queryClient}>
-            <Helmet helmetData={helmetData}>
-                <meta
-                    property='og:title'
-                    content='R10 Score - Resultados en vivo'
-                />
-                <meta property='og:type' content='website' />
-                <meta
-                    property='og:url'
-                    content={'https://erick-rimac-seguros.vercel.app/'}
-                />
-                <meta
-                    property='og:image'
-                    content={`${'https://erick-rimac-seguros.vercel.app/'}/r10.webp`}
-                />
-                <meta property='og:image:type' content='image/webp' />
-                <meta property='og:image:width' content='600' />
-                <meta property='og:image:height' content='315' />
-                <meta
-                    property='og:image:alt'
-                    content='Una tienda virtual con acceso a login'
-                />
-                <meta property='og:site_name' content='R10 Score' />
-                <meta
-                    property='og:image:secure_url'
-                    content={`${'https://erick-rimac-seguros.vercel.app/'}/r10.webp`}
-                />
-                <meta property='og:locale' content='es' />
-            </Helmet>
-            <RouterProvider router={allRoutes} />
-        </QueryClientProvider>
+        <HelmetProvider>
+            <QueryClientProvider client={queryClient}>
+                <Helmet helmetData={helmetData}>
+                    <meta
+                        property='og:title'
+                        content='R10 Score - Resultados en vivo'
+                    />
+                    <meta property='og:type' content='website' />
+                    <meta
+                        property='og:url'
+                        content={'https://erick-rimac-seguros.vercel.app/'}
+                    />
+                    <meta
+                        property='og:image'
+                        content={`${'https://erick-rimac-seguros.vercel.app/'}/r10.webp`}
+                    />
+                    <meta property='og:image:type' content='image/webp' />
+                    <meta property='og:image:width' content='600' />
+                    <meta property='og:image:height' content='315' />
+                    <meta
+                        property='og:image:alt'
+                        content='Una tienda virtual con acceso a login'
+                    />
+                    <meta property='og:site_name' content='R10 Score' />
+                    <meta
+                        property='og:image:secure_url'
+                        content={`${'https://erick-rimac-seguros.vercel.app/'}/r10.webp`}
+                    />
+                    <meta property='og:locale' content='es' />
+                </Helmet>
+                <RouterProvider router={allRoutes} />
+            </QueryClientProvider>
+        </HelmetProvider>
     )
 }
 
