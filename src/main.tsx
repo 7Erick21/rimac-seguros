b08258fd771a4a83696c35a6e42a4088d32f1871
pinedacuevaerick910 <pinedacuevaerick910@gmail.com>
2023-12-18 14:27:16 -0500

    Meta OG SEO

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import '@/styles/theme.scss'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'
 
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <HelmetProvider context={{}}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+        ,
+    </HelmetProvider>,
 )
